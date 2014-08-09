e3904510e709b6e137faad8fc0c6442753d97d7a
Christopher B. Choy <chrischoy@rescomp-13-283185.stanford.edu>
2014-08-09 02:59:33 -0700

    sm13 compilation, before refactoring

--- cuda_compile.m
+++ cuda_compile.m
@@ -1,8 +1,10 @@
 function cuda_compile( func_name, matlabroot, cudaroot, optimize)
 if nargin == 3
   optimize = true;
-end  
+end
 
+gpuInfo = gpuDevice
+str2num(gpuInfo.ComputeCapability)
 % if ~verLessThan('matlab', '8.0.1')
 %   % http://www.mathworks.com/help/distcomp/run-mex-functions-containing-cuda-code.html
 %   setenv('MW_NVCC_PATH',[cudaroot '/nvcc'])
@@ -11,9 +13,9 @@ end
   eval(['!rm ' func_name '.o']);
 
   if optimize
-    eval(sprintf('!nvcc -O3 -DNDEBUG -Xcompiler -fPIC -v -I%s/extern/include -I%s/toolbox/distcomp/gpu/extern/include -c %s.cu', matlabroot, matlabroot, func_name));
+    eval(sprintf('!nvcc -O3 -DNDEBUG -arch=sm_13 -ftz=true -prec-div=false -prec-sqrt=false -Xcompiler -fPIC -v -I%s/extern/include -I%s/toolbox/distcomp/gpu/extern/include -c %s.cu', matlabroot, matlabroot, func_name));
   else
-    eval(sprintf('!nvcc -O3 -DNDEBUG -Xcompiler -Wall -Xcompiler -Wextra -Xcompiler -fPIC -v -I%s/extern/include -I%s/toolbox/distcomp/gpu/extern/include -c %s.cu', matlabroot, matlabroot, func_name));
+    eval(sprintf('!nvcc -arch=sm_13 -ftz=true -prec-div=false -prec-sqrt=false -Xcompiler -Wall -Xcompiler -Wextra -Xcompiler -fPIC -v -I%s/extern/include -I%s/toolbox/distcomp/gpu/extern/include -c %s.cu', matlabroot, matlabroot, func_name));
   end
   if ismac
     eval(['mex -largeArrayDims ' func_name '.o -I' cudaroot '/include -L' matlabroot '/bin/maci64  -lcudart -lcufft -lmwgpu']);  
--- demoConvFFTData.m
+++ demoConvFFTData.m
@@ -8,10 +8,12 @@ cos(gpuArray(1));
 
 MATLAB_ROOT = '/afs/cs/package/matlab-r2013b/matlab/r2013b/';
 CUDA_ROOT = '/usr/local/cuda-6.0/';
+
 if ismac
   MATLAB_ROOT = '/Applications/MATLAB_R2014a.app/';
   CUDA_ROOT = '/usr/local/cuda/';
 end
+
 cuda_compile('cudaConvFFTData',MATLAB_ROOT, CUDA_ROOT, 1)
 
 clear;
