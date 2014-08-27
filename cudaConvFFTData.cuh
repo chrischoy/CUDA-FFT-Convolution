32842f83d688b3cdef578247d5da607295171397
Christopher B. Choy <chrischoy208@gmail.com>
2014-08-26 23:25:16 -0700

    Reduction in progress

--- cudaConvFFTData.cuh
+++ cudaConvFFTData.cuh
@@ -88,6 +88,123 @@ __global__ void sumAlongFeatures(
         }
     }
 }
+
+
+/*
+If a variable located in global or shared memory is declared as volatile, 
+the compiler assumes that its value can be changed or used at any time by 
+another thread and therefore any reference to this variable compiles to an
+ actual memory read or write instruction.
+*/
+__global__ void sumAlongFeaturesReduction(
+    float *convolutionResult,
+    const volatile float *convolutionPerFeature,
+    int FFT_H,
+    int FFT_W,
+    int FEATURE_DIM,
+    int PREV_POW_2_FEATURE_DIM
+){
+    const int x = IMUL(blockDim.x, blockIdx.x) + threadIdx.x;
+    const int y = IMUL(blockDim.y, blockIdx.y) + threadIdx.y;
+    const int z = IMUL(blockDim.z, blockIdx.z) + threadIdx.z;
+
     
+    if(x < FFT_W && y < FFT_H && z < PREV_POW_2_FEATURE_DIM){
+        const int result_i = IMUL(FFT_H, x) + y;
+        const int N = IMUL(FFT_W, FFT_H);
+        uint idx = PREV_POW_2_FEATURE_DIM;
+        while(idx >= 1){
+            idx = idx >> 1;
+            convolutionPerFeature[result_i + IMUL(z, N)] += (z+idx < FEATURE_DIM)?convolutionPerFeature[result_i + IMUL(z + idx, N)]:0;
+            __synchronizeThread();
+        }
+        convolutionResult[result_i] = convolutionPerFeature[result_i];
+    }
+}
+
+
+__device__ int getGlobalIdx_2D_Grid_3D_Block()
+{
+    int blockId = blockIdx.x 
+             + blockIdx.y * gridDim.x; 
+    int threadId = blockId * (blockDim.x * blockDim.y * blockDim.z)
+               + (threadIdx.z * (blockDim.x * blockDim.y))
+               + (threadIdx.y * blockDim.x)
+               + threadIdx.x;
+    return threadId;
+} 
+
+/* Unrolled version of reduction */
+__global__ void sumAlongFeaturesReductionUnroll32(
+    float *convolutionResult,
+    const volatile float *convolutionPerFeature,
+    int FFT_H,
+    int FFT_W,
+    int N,
+    int FEATURE_DIM
+){
+    extern __shared__ int sdata[];
+    const int x = IMUL(blockDim.x, blockIdx.x) + threadIdx.x;
+    const int y = IMUL(blockDim.y, blockIdx.y) + threadIdx.y;
+    const int z = IMUL(blockDim.z, blockIdx.z) + threadIdx.z;
+    
+    if(x < FFT_W && y < FFT_H && z < 16){
+        const unsigned int SharedN = IMUL(blockDim.x, blockDim.y);
+        const unsigned int xyTid = IMUL(threadIdx.y, blockDim.x)
+                                    + threadIdx.x;
+        const int tid       = IMUL(threadIdx.z, SharedN) + xyTid;
+        const int result_i  = IMUL(FFT_H, x) + y;
+
+        sdata[tid] = convolutionPerFeature[result_i + IMUL(z, N)] + (z + 16 < FEATURE_DIM) ? convolutionPerFeature[result_i + IMUL(z + 16, N)] : 0;
+        __synchronizeThread();
+        sdata[tid] +=sdata[tid + IMUL( 8, SharedN)];
+        sdata[tid] +=sdata[tid + IMUL( 4, SharedN)];
+        sdata[tid] +=sdata[tid + IMUL( 2, SharedN)];
+        sdata[tid] +=sdata[tid + SharedN];
+        if (threadIdx.z == 0){
+            convolutionResult[result_i] = sdata[xyTid];
+        }
+    }
+}
+
+
+/* Instructions are SIMD synchronous within a warp */
+template <unsigned int blockSize>
+__device__ void warpReduce(volatile int *sdata, unsigned int tid) {
+    if (blockSize >= 64) sdata[tid] += sdata[tid + 32];
+    if (blockSize >= 32) sdata[tid] += sdata[tid + 16];
+    if (blockSize >= 16) sdata[tid] += sdata[tid + 8];
+    if (blockSize >=  8) sdata[tid] += sdata[tid + 4];
+    if (blockSize >=  4) sdata[tid] += sdata[tid + 2];
+    if (blockSize >=  2) sdata[tid] += sdata[tid + 1];
+}
+
+// unsigned int sharedSize = numThreads*sizeof(int);
+template <unsigned int blockSize>
+__global__ void reduce6(int *g_idata, int *g_odata, unsigned int n) {
+    extern __shared__ int sdata[];
+    unsigned int tid = threadIdx.x;
+    unsigned int i = blockIdx.x*(blockSize*2) + tid; 
+    unsigned int gridSize = blockSize*2*gridDim.x; sdata[tid] = 0;
+    while (i < n) {
+        sdata[tid] += g_idata[i] + g_idata[i+blockSize]; 
+        i += gridSize; 
+    }
+    __syncthreads();
+    if (blockSize >= 512) { 
+        if (tid < 256) sdata[tid] += sdata[tid + 256];
+        __syncthreads(); 
+    } 
+    if (blockSize >= 256) { 
+        if (tid < 128) sdata[tid] += sdata[tid + 128]; 
+        __syncthreads(); 
+    } 
+    if (blockSize >= 128) { 
+        if (tid < 64) sdata[tid] += sdata[tid + 64]; 
+        __syncthreads();
+    }
+    if (tid < 32) warpReduce(sdata, tid);
+    if (tid == 0) g_odata[blockIdx.x] = sdata[0]; 
+}    
 
 #endif
